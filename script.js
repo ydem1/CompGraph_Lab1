d592d5f05620757a0f889d38290a5992fe2d1829
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2023-10-18 15:57:18 +0300

    second version

--- script.js
+++ script.js
@@ -4,13 +4,25 @@ const context = canvas.getContext('2d');
 canvas.width = 500;
 canvas.height = 500;
 
-let x = 100;
-let y = 100;
+const widthFigur = 80;
+const heightFigur = 100;
+
+let x = canvas.width / 2 - widthFigur / 2;
+let y = canvas.height / 2 - heightFigur / 2;
+
+const centerX = x + widthFigur;
+const centerY = y + heightFigur;
 
 let rotation = 0;
+let rotatePointX = 0;
+let rotatePointY = 0;
+
 let shiftX = 0;
 let shiftY = 0;
-let scale = 1; 
+
+let scaleX = 1;
+let scaleY = 1;
+
 let isSymmetry = false;
 
 const points = [];
@@ -29,9 +41,6 @@ points.push({ x: x + 80, y: y + 80 });
 points.push({ x: x, y: y + 80 });
 points.push({ x: x + 80, y: y + 80 });
 points.push({ x: x + 80, y: y });
-
-const centerX = x + 60; // Горизонтальна координата центра фігури
-const centerY = y + 100; // Вертикальна координата центра фігури
 
 function drawGrid() {
   const gridSize = 20;
@@ -77,15 +86,20 @@ function drawLine(x1, y1, x2, y2) {
   context.lineTo(x2, y2);
 }
 
-function drawCircle(x, y, radius, startAngle, endAngle) {
-  context.arc(x, y, radius, startAngle, endAngle);
+function drawCircle(x, y, radiusX, radiusY, startAngle, endAngle) {
+  context.ellipse(x, y, radiusX, radiusY, 0, startAngle, endAngle);
 }
 
-function rotatePoint(x, y, originX, originY, angle) {
+function rotatePoint(x, y, angle) {
+  const xOffset = x - rotatePointX;
+  const yOffset = y - rotatePointY;
+
   const cosA = Math.cos(angle);
   const sinA = Math.sin(angle);
-  const newX = cosA * (x - originX) - sinA * (y - originY) + originX;
-  const newY = sinA * (x - originX) + cosA * (y - originY) + originY;
+
+  const newX = cosA * xOffset - sinA * yOffset + rotatePointX;
+  const newY = sinA * xOffset + cosA * yOffset + rotatePointY;
+
   return { x: newX, y: newY };
 }
 
@@ -96,12 +110,12 @@ function shiftPoint(dx, dy, originX, originY) {
   return { x: newX, y: newY };
 }
 
-function scalePoint(x, y, centerX, centerY, scale) {
+function scalePoint(x, y) {
   const dx = x - centerX;
   const dy = y - centerY;
 
-  const scaledDx = dx * scale;
-  const scaledDy = dy * scale;
+  const scaledDx = dx * scaleX;
+  const scaledDy = dy * scaleY;
 
   const scaledX = centerX + scaledDx;
   const scaledY = centerY + scaledDy;
@@ -109,10 +123,16 @@ function scalePoint(x, y, centerX, centerY, scale) {
   return { x: scaledX, y: scaledY };
 }
 
-function symmetryPoint(originX) {
+function symmetryPointX(originX) {
   const newX = 2 * centerX - originX;
 
   return newX;
+}
+
+function symmetryPointY(originY) {
+  const newY = 2 * centerY - originY;
+
+  return newY;
 }
 
 function drawFigure() {
@@ -125,12 +145,12 @@ function drawFigure() {
   context.lineWidth = 2;
 
   const newPoints = points.map(point => {
-    const { x: rotateX, y: rotateY } = rotatePoint(point.x, point.y, centerX, centerY, rotation);
-    const { x: scaledX, y: scaledY } = scalePoint(rotateX, rotateY, centerX, centerY, scale);
+    const { x: rotateX, y: rotateY } = rotatePoint(point.x, point.y, rotation);
+    const { x: scaledX, y: scaledY } = scalePoint(rotateX, rotateY);
     let { x: newX, y: newY } = shiftPoint(scaledX, scaledY, shiftX, shiftY);
 
     if (isSymmetry) {
-      newX = symmetryPoint(newX);
+      newX = symmetryPointX(newX);
     }
 
     return { x: newX, y: newY };
@@ -154,43 +174,24 @@ function drawFigure() {
   const endSmallCircle = 2 * Math.PI;
   const radiusSmallCircle = 20;
 
-  const { x: rotateX, y: rotateY } = rotatePoint(x, y + 40, centerX, centerY, rotation);
-  const { x: scaledX, y: scaledY } = scalePoint(rotateX, rotateY, centerX, centerY, scale);
+  const { x: rotateX, y: rotateY } = rotatePoint(x, y + 40, rotation);
+  const { x: scaledX, y: scaledY } = scalePoint(rotateX, rotateY);
   let { x: circleX, y: circleY } = shiftPoint(scaledX, scaledY, shiftX, shiftY);
 
   if (isSymmetry) {
-    circleX = symmetryPoint(circleX);
+    circleX = symmetryPointX(circleX);
   }
 
   const symmetry = isSymmetry === true ? -1 : 1; 
 
   context.beginPath();
-  drawCircle(circleX, circleY, radiusBigCircle * scale, symmetry * (startBigCircle + rotation), symmetry * (endBigCircle + rotation));
+  drawCircle(circleX, circleY, radiusBigCircle * scaleX, radiusBigCircle * scaleY, symmetry * (startBigCircle + rotation), symmetry * (endBigCircle + rotation));
   context.stroke();
 
   context.beginPath();
-  drawCircle(circleX, circleY, radiusSmallCircle * scale, startSmallCircle, endSmallCircle);
+  drawCircle(circleX, circleY, radiusSmallCircle * scaleX, radiusSmallCircle * scaleY, startSmallCircle, endSmallCircle);
   context.stroke();
 }
-
-document.getElementById('translateBtn').addEventListener('click', () => {
-  shiftX += 20;
-  shiftY += 20;
-
-  drawFigure();
-});
-
-document.getElementById('rotateBtn').addEventListener('click', () => {
-  rotation += Math.PI / 6;
-
-  drawFigure();
-});
-
-document.getElementById('scaleBtn').addEventListener('click', () => {
-  scale *= 1.2;
-
-  drawFigure();
-});
 
 document.getElementById('symmetryBtn').addEventListener('click', () => {
   isSymmetry = !isSymmetry;
@@ -198,5 +199,50 @@ document.getElementById('symmetryBtn').addEventListener('click', () => {
   drawFigure();
 });
 
+
+const euclideanTransformationsForm = document.getElementById("euclidean-transformations__form");
+
+euclideanTransformationsForm.addEventListener("submit", function(event) {
+  event.preventDefault(); // Зупинити стандартну дію форми (у цьому випадку, перезавантаження сторінки)
+
+  const moveXValue = parseInt(document.getElementById("moveX").value) || 0;
+  const moveYValue = parseInt(document.getElementById("moveY").value) || 0;
+
+  const rotateXValue = parseInt(document.getElementById("RotateX").value) || 0;
+  const rotateYValue = parseInt(document.getElementById("RotateY").value) || 0;
+
+  const rotateAngle = parseInt(document.getElementById("Angle").value) || 0;
+
+  shiftX += moveXValue;
+  shiftY += moveYValue;
+
+  rotatePointX = rotateXValue;
+  rotatePointY = rotateYValue;
+
+  rotation += rotateAngle * Math.PI / 180;
+
+  drawFigure();
+});
+
+const athenianTransformationsForm = document.getElementById("athenian-transformations__form");
+
+athenianTransformationsForm.addEventListener("submit", function(event) {
+  event.preventDefault(); // Зупинити стандартну дію форми (у цьому випадку, перезавантаження сторінки)
+
+  const scaleXValue = parseInt(document.getElementById("scaleX").value) || 0;
+  const scaleYValue = parseInt(document.getElementById("scaleY").value) || 0;
+
+  scaleX += scaleXValue / 10;
+  scaleY += scaleYValue / 10;
+  
+  const minScale = 0.1;
+  const maxScale = 10;
+  
+  scaleX = Math.min(maxScale, Math.max(minScale, scaleX));
+  scaleY = Math.min(maxScale, Math.max(minScale, scaleY));
+
+
+  drawFigure();
+});
 
 drawFigure();
